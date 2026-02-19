3cb79a7bccbf63cfc4e00971ff3ba08a55a6602c
badVibes-- <badVibes--@users.noreply.github.com>
2026-02-19 23:20:42 +0100

    Update IMSlider.cpp

--- src/slic3r/GUI/IMSlider.cpp
+++ src/slic3r/GUI/IMSlider.cpp
@@ -1470,10 +1470,10 @@ void IMSlider::on_mouse_wheel(wxMouseEvent& evt) {
 
     ImGuiWrapper& imgui = *wxGetApp().imgui();
 
-    float wheel = 0.0f;
-    wheel = evt.GetWheelRotation() > 0 ? 1.0f : -1.0f;
+    float wheel = evt.GetWheelRotation();
     if (wheel == 0.0f)
         return;
+    wheel = wheel > 0 ? 1.0f : -1.0f;
 
 #ifdef __WXOSX__
     if (wxGetKeyState(WXK_SHIFT)) {
